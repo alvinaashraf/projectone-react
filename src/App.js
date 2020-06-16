c4a17cd8efd2ef76cf10d7abb09638fa29c83a14
alvinaashraf <57407049+alvinaashraf@users.noreply.github.com>
2020-06-16 19:19:21 +0500

    props

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import './dinner';
+import Dinner from './dinner';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.hhhh by aliens !!!! abc12344
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="Dinner">
+    <Dinner dishname="biryani"  sweet="ice creans" age = {20}  >
+
+      </Dinner>
+
+      <Dinner sweet="chocloate ">
+
+        </Dinner>
+    
+         </div>
+   
   );
 }
 
+
+
+
 export default App;
+
---
+++ src/dinner.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Dinner(props){
+
+return(
+
+
+    <h1>hhhhhskkksksksk {props.dishname} {props.sweet} {props.age -2}
+        </h1> 
+
+
+)
+
+
+
+}
+
+
+export default Dinner;
